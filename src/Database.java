f7952b5a7a8d692aefddbda230f783c4bb50bc5d
JtHove <s1094757@windesheim.nl>
2019-05-08 16:03:17 +0200

    Added DBtest, Changed Database
    Statements en Query's kunnen nu gemaakt en uitgevoerd worden.

--- src/Database.java
+++ src/Database.java
@@ -1,36 +1,64 @@
-//import java.sql.*;
-//
-//public class Database {
-//    private String url = "jdbc:mysql://localhost/wideworldimporters";
-//    private String username = "root", password = "";
-//    public static Connection c;
-//
-//    public void openConnection() {
-//        c = DriverManager.getConnection(url, username, password);
-//    }
-//
-//    public void closeConnection() {
-//        c.close();
-//    }
-//}
+
 import java.sql.*;
 
 public class Database {
     private static String url = "jdbc:mysql://localhost/wideworldimporters";
     private static String username = "root", password = "";
-    public static Connection c;
+    private static Connection c;
+    private static Statement s;
 
-    public static void openConnection() {
+    public static boolean openConnection() {
         try {
             c = DriverManager.getConnection(url, username, password);
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public static boolean closeConnection() {
+        try {
+            c.close();
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public static void createStatement() {
+        try {
+            s = c.createStatement();
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
-    public void closeConnection() {
+    public static ResultSet executeQuery(String Query) {
         try {
-            c.close();
+            ResultSet rs = s.executeQuery(Query);
+            return rs;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Er is iets mis met de database connectie");
+            return null;
+        }
+    }
+
+    public static int executeUpdate(String Query) {
+        try {
+            int succes = s.executeUpdate(Query);
+            return succes;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return 0;
+        }
+    }
+
+    public static void closeStatement() {
+        try {
+            s.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
