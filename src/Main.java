c28c39482306e494e73c390e652aa7831f17c3ac
Ayak26 <s1129531@windesheim.nl>
2019-05-15 12:34:07 +0200

    CHANGE Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,16 @@
-package src;
 
+import java.awt.*;
 import javax.swing.*;
+import java.awt.event.*;
+import java.awt.Dimension;
+import java.net.URL;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 
 public class Main {
-    public static void main(String [] args) {
-        Screen screen = new Screen();
+
+    public static void main(String[] args) {
+        Screen homescreen = new Screen();
+     //   JPanel_MainMenu JPanel_MainMenu = new JPanel_MainMenu();
     }
 }
