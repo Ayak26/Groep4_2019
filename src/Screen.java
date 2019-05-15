71c51385dab656b1a4ad4384ef58bd6438c0572a
Ayak26 <s1129531@windesheim.nl>
2019-05-15 12:33:35 +0200

    CHANGE Screen.java

--- src/Screen.java
+++ src/Screen.java
@@ -1,91 +1,79 @@
-package src;
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-import java.awt.*;
 import javax.swing.*;
-import java.awt.event.*;
-import java.awt.Dimension;
-import java.net.URL;
-import java.io.IOException;
-import javax.imageio.ImageIO;
+import java.awt.*;
 
-/**
- * @author Alihan
- */
-public class Screen extends JFrame implements ActionListener {
-    private JButton sortBotButton;
-    private JButton packBotButton;
-    private JButton orderButton;
-    private JButton stockButton;
-    private JButton menuButton;
+public class Screen extends JFrame {
+    //  JFrame hoofdmenu = new JFrame("HMI applicatie");
+    JButton sorting_robot_button;
+    JButton packing_robot_button;
+    JButton order_button;
+    JButton stock_button;
+    JPanel mainmenu_panel;
 
-    private JPanel panel;
-    private JPanel emptypanel;
+    JPanel mainmenu_panel_image;
+    JLabel image_sortingrob;
+    JLabel image_packingrob;
+    JLabel image_order;
+    JLabel image_stock;
 
-    private JLabel labelI;
-
-    private Image image;
 
     public Screen() {
+        setLayout(null);
         setSize(800, 480);
-//        setLocation(0, 0);
+        setLocation(0, 0);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//        setDefaultLookAndFeelDecorated(true);
-        setLayout(new GridLayout(2, 4));
+        setDefaultLookAndFeelDecorated(true);
+        //initializing
+        sorting_robot_button = new JButton("Sorteerrobot");
+        packing_robot_button = new JButton("Inpakrobot");
+        order_button = new JButton("Orders");
+        stock_button = new JButton("Voorraad");
+        mainmenu_panel = new JPanel();
 
-        menuButton = new JButton("Menu");
-        menuButton.setBounds(0, 0, 100, 60);
+        //initializing for images
+        mainmenu_panel_image = new JPanel();
+        image_sortingrob = new JLabel();
+        image_packingrob = new JLabel();
+        image_order = new JLabel();
+        image_stock = new JLabel();
 
-        panel = new JPanel();
-        panel.setLayout(null);
-        panel.add(menuButton);
-//        add(panel);
-        add(new JPanel());
-        add(new JPanel());
-        add(new JPanel());
-        add(new JPanel());
+        //adding buttons
+        mainmenu_panel.add(sorting_robot_button);
+        mainmenu_panel.add(packing_robot_button);
+        mainmenu_panel.add(order_button);
+        mainmenu_panel.add(stock_button);
 
-        try {
-            URL url = new URL("https://image.flaticon.com/icons/png/512/17/17969.png");
-            image = ImageIO.read(url);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        //adding images
+        mainmenu_panel_image.add(image_sortingrob);
+        mainmenu_panel_image.add(image_packingrob);
+        mainmenu_panel_image.add(image_order);
+        mainmenu_panel_image.add(image_stock);
 
-        labelI = new JLabel(new ImageIcon(image));
+        //adding actual images to the labels
+        image_sortingrob.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/Afbeelding1.png")));
+        image_packingrob.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/Afbeelding2.png")));
+        image_order.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/Afbeelding3.png")));
+        image_stock.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/Afbeelding4.png")));
 
-        sortBotButton = new JButton("Sorteerrobot");
-        add(sortBotButton);
-//        sortBotButton.setBounds(300, 500, 200, 30);
-        sortBotButton.addActionListener(this);
+        //setting size for buttons
+        sorting_robot_button.setPreferredSize(new Dimension(150, 40));
+        packing_robot_button.setPreferredSize(new Dimension(150, 40));
+        order_button.setPreferredSize(new Dimension(150, 40));
+        stock_button.setPreferredSize(new Dimension(150, 40));
 
-        packBotButton = new JButton("Inpakrobot");
-        add(packBotButton);
-//        packBotButton.setBounds(550, 500, 200, 30);
-        packBotButton.addActionListener(this);
+        //setting size for images
+        image_sortingrob.setPreferredSize(new Dimension(150, 150));
+        image_packingrob.setPreferredSize(new Dimension(150, 150));
+        image_order.setPreferredSize(new Dimension(150, 150));
+        image_stock.setPreferredSize(new Dimension(150, 150));
 
-        orderButton = new JButton("Orders");
-        add(orderButton);
-//        orderButton.setBounds(800, 500, 200, 30);
+        //setting size/position for panels
+        mainmenu_panel.setBounds(0, 270, 800, 150);
+        mainmenu_panel_image.setBounds(22, 130, 800, 200);
 
-        stockButton = new JButton("Voorraad");
-        add(stockButton);
-//        stockButton.setBounds(1050, 500, 200, 30);
-
+        //adding panels to frame
+        add(mainmenu_panel);
+        add(mainmenu_panel_image);
         setVisible(true);
-    }
 
-    public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == sortBotButton) {
-            this.dispose();
-//            new Sorteerrobot();
-        } else if (e.getSource() == packBotButton) {
-            this.dispose();
-//            new Inpakrobot();
-        }
     }
 }
