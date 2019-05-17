8d15c1df4d029be00dde741cffbddd1f6f59e076
DamianAudier <damianaudier@gmail.com>
2019-05-17 11:00:42 +0200

    ADD Orderaanmaken.java, Orderspecifiek.java UPDATE Home.java, Orderaanmaken.fxml, Orders.java, Orders.fxml, Orderspecifiek.fxml, SelectPackingRobot.fxml, Selectproduct.java, Selectproduct.fxml, SortRobot.fxml

--- src/gui/home/Orders.java
+++ src/gui/home/Orders.java
@@ -1,9 +1,14 @@
 package gui.home;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -11,7 +16,26 @@ import java.util.ResourceBundle;
 public class Orders implements Initializable {
 
     @FXML
+    private Button home, createorder;
+
+    @FXML
     private ImageView start_stop;
+
+
+
+    @FXML
+    private void goHome() throws Exception {
+        Stage stage = (Stage)home.getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("Home.fxml"));
+        stage.setScene(new Scene(root));
+    }
+
+    @FXML
+    private void goCreateorder() throws Exception {
+        Stage stage = (Stage)createorder.getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("Orderaanmaken.fxml"));
+        stage.setScene(new Scene(root));
+    }
 
     @FXML
     private void start_stop() {
