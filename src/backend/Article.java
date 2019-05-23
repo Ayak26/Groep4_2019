a18eb4aa056edc3007d7875cac1c7d1297b274d1
Unknown <arjan-v.diest@hotmail.com>
2019-05-23 09:50:39 +0200

    Add comments to the article class

--- src/backend/Article.java
+++ src/backend/Article.java
@@ -11,9 +11,11 @@ public class Article {
 
     public Article(int id) {
         this.id = id;
+
         Database.createStatement();
         ResultSet rs = Database.executeQuery("SELECT StockItemName, Size, ColorName FROM stockitems JOIN colors " +
                 "ON stockitems.ColorID = colors.ColorID WHERE StockItemID = " + id);
+
         if (rs != null) {
             try {
                 while (rs.next()) {
@@ -31,10 +33,20 @@ public class Article {
         Database.closeStatement();
     }
 
+    /**
+     * return article id
+     *
+     * @return int
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * returns a string composed of the id, name, colour and size of the article
+     *
+     * @return String
+     */
     @Override
     public String toString() {
         return "backend.Article{" +
@@ -45,15 +57,29 @@ public class Article {
                 '}';
     }
 
-
+    /**
+     * return the name of the article
+     *
+     * @return String
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * return the colour of the article
+     *
+     * @return String
+     */
     public String getColour() {
         return colour;
     }
 
+    /**
+     * return the size of the article
+     *
+     * @return int
+     */
     public int getSize() {
         return size;
     }
