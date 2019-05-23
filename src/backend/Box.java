eb810316a2db48f2d2c096a85abf3bb282978091
Unknown <arjan-v.diest@hotmail.com>
2019-05-23 10:28:36 +0200

    Add comments to box class

--- src/backend/Box.java
+++ src/backend/Box.java
@@ -12,18 +12,38 @@ public class Box {
         this.name = name;
     }
 
+    /**
+     * Set the size of this box
+     *
+     * @param size the size you want this box to be
+     */
     public void setSize(int size){
         this.size = size;
     }
 
+    /**
+     * set the name of this box
+     *
+     * @param name the name you want this box to have
+     */
     public void setName(String name){
         this.name = name;
     }
 
+    /**
+     * add an article to the contents of this box
+     *
+     * @param article the article which you want to be in the box
+     */
     public void addContent(Article article){
         content.add(article);
     }
 
+    /**
+     * calculate the amount of space is left in this box
+     *
+     * @return an integer with the amount of space that the box has left
+     */
     public Integer spaceLeft() {
         int space = size;
         for (Article article: content) {
@@ -33,6 +53,10 @@ public class Box {
     }
 
 
+
+    /**
+     * @return a string composed of the name, content and size of the box
+     */
     @Override
     public String toString() {
         return "Box "+ name +"{" +
@@ -41,6 +65,11 @@ public class Box {
                 '}';
     }
 
+    /**
+     * check if the space that is left is equal to the max size of the box
+     *
+     * @return a boolean containing whether the box is empty or not
+     */
     boolean empty() {
         return size == this.spaceLeft();
     }
